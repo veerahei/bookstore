4464c0079fca32df7be8e87fd802ee6d44f36418
Veera <veera.heikkinen95@gmail.com>
2025-01-31 14:09:38 +0200

    Booklist page added

--- bookstore/src/main/java/k25/bookstore/web/BookController.java
+++ bookstore/src/main/java/k25/bookstore/web/BookController.java
@@ -1,15 +1,30 @@
 package k25.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import k25.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
+    private BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/index")
     // Method to be defined later
     public String bookMethod() {
-        return null;
+        return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String booklist(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 
 }
