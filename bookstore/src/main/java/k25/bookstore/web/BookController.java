389e7495962066d87d584645aeccbd1e2d6c80b5
Veera <veera.heikkinen95@gmail.com>
2025-02-01 16:39:31 +0200

    Added edit-functionality

--- bookstore/src/main/java/k25/bookstore/web/BookController.java
+++ bookstore/src/main/java/k25/bookstore/web/BookController.java
@@ -47,4 +47,10 @@ public class BookController {
         repository.deleteById(bookId);
         return "redirect:../booklist";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", repository.findById(bookId));
+        return "editBook";
+    }
 }
