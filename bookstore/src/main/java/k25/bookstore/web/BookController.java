b953281435476e46241529f308acfdcd2ab6f78d
Veera <veera.heikkinen95@gmail.com>
2025-02-01 16:01:22 +0200

    Added crud (create and delete) functionalities

--- bookstore/src/main/java/k25/bookstore/web/BookController.java
+++ bookstore/src/main/java/k25/bookstore/web/BookController.java
@@ -3,7 +3,10 @@ package k25.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import k25.bookstore.domain.Book;
 import k25.bookstore.domain.BookRepository;
 
 @Controller
@@ -27,4 +30,21 @@ public class BookController {
         return "booklist";
     }
 
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
 }
